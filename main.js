2b4fa9dc344fd590a425794adc418a861db58ee4
binoy <binoy@mobme.in>
2016-04-06 13:02:27 +0530

    Logic to add first level nodes

--- main.js
+++ main.js
@@ -1,2 +1,59 @@
-$('#main').attr('contentEditable','true');
-$('#main').focus();
+var ID = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+var rootNode = new Node(null, null);
+var objectTable = {};
+function addToObjectTable(node) {
+  objectTable[node.id] = node;
+};
+addToObjectTable(rootNode);
+
+
+
+function Node(data, parentId = null) {
+  this.id = ID();
+  this.parentId = parentId;
+  this.data = data;
+  this.children = [];
+}
+
+function createChildFor(parentNode) {
+  var child = new Node('', parentNode.id);
+  parentNode.children.push(child);
+  addToObjectTable(child);
+  return child;
+};
+
+function render(node) {
+  var nodeDiv = $('<div />');
+  nodeDiv.attr('id', node.id);
+  nodeDiv.attr('contentEditable','true');
+  $('#main').append(nodeDiv);
+  nodeDiv.focus();
+
+  nodeDiv.blur(function() {
+    objectTable[node.id].data = nodeDiv.text();
+    console.log(JSON.stringify(rootNode));
+  });
+
+  nodeDiv.keydown(function(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      event.stopPropagation();
+      var parentNode = objectTable[node.parentId];
+      var newNode = createChildFor(parentNode);
+      render(newNode);
+    }
+  });
+}
+
+
+var firstNode = createChildFor(rootNode);
+render(firstNode);
+
+$('#main div').focus();
+
--- todo.md
+++ todo.md
@@ -1,2 +1,13 @@
-- basic page
+Todos:
+- Better fonts
+- Try changing div to ul and li
+- css positioning to render tabbed node
+
+Done:
+- Create a basic node on load
+- Enter two lines and create 2 nodes
+
+Known bugs:
+
+Enhancements:
 - http://www.webdevbreak.com/episodes/zen-textarea-pure-css
